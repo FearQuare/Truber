528dd7ff133d72de7dea5384026152108ef81f9b
Murat Vermez <murat.vermez@std.izmirekonomi.edu.tr>
2023-12-28 22:54:50 +0300

    complete login page

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:truber/screens/profile_page.dart';
 import 'package:truber/widgets/navigation_bar.dart';
+import 'package:truber/screens/login_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Truber Home Page'),
+      home: LoginScreen(),
     );
   }
 }
